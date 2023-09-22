792359711d3060e5c37b35163a9b156d123c7f19
Jieun Lee <jieun98.dev@gmail.com>
2023-09-22 09:31:03 +0900

    탭 수정하기

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -3,37 +3,47 @@ import Logo from "../../assets/images/logo.svg";
 import shareIcon from "../../assets/images/ic_share.svg";
 
 const TopNav = () => {
-    const APP_DOWNLOAD_LINK =
-        "https://open.shinhansec.com/phone/goM.jsp?p=OTUxMCYmJiZTJiY*&amp;v=2";
+  const APP_DOWNLOAD_LINK =
+    "https://open.shinhansec.com/phone/goM.jsp?p=OTUxMCYmJiZTJiY*&amp;v=2";
 
-    const onClickShareBtn = () => {
-        //T0D0: (미션] 현재 사이트 링크 클립보드 복사하고 "복사되었습니다" 알럿 띄우기
-        const currentUrl = window. document. location.href;
-        navigator.clipboard
-            .writeText (currentUrl)
-            .then(() => {
-                alert ("주소가 복사되었습니다. ") ;
-            })
-            .catch((err) => alert(" 실패했습니다."))
-    };
+  const onClickCopyButton = () => {
+    //T0D0: (미션] 현재 사이트 링크 클립보드 복사하고 "복사되었습니다" 알럿 띄우기
+    const currentUrl = window. document. location.href;
+    navigator.clipboard
+      .writeText (currentUrl)
+      .then(() => {
+        alert ("주소가 복사되었습니다. ") ;
+      })
+      .catch((err) => alert(" 실패했습니다."))
+  };
 
-    return (
-        <div className={styles.container}>
-            {/* 로고 */}
-            <img src={Logo} alt="logo" />
-            <div className={styles.rightBtnContainer}>
-                {/*앱 다운로드 버튼 */}
-                <a href={APP_DOWNLOAD_LINK} target="_blank">
-                    <div className={styles.roundBtn}>앱 다운로드</div>
-                </a>
+  return (
+    <div>
+      <div className={styles.container}>
+        {/* 로고 */}
+        <img src={Logo} alt="logo" />
+        <div className={styles.rightButtonContainer}>
+          {/*앱 다운로드 버튼 */}
+          <a href={APP_DOWNLOAD_LINK} target="_blank">
+            <div className={styles.roundButton}>앱 다운로드</div>
+          </a>
 
-                {/* 공유 버튼 */}
-                <div className={styles.shareBtn} onClick={onClickShareBtn}>
-                <img src={shareIcon} alt="share" />
-            </div>
+          {/* 공유 버튼 */}
+          <div className={styles.circleButton} onClick={onClickCopyButton}>
+            <img src={shareIcon} alt="share" />
+          </div>
         </div>
+      </div>
+      <div className={styles.container2}>
+        <div className={styles.tab}>
+          국내주식
+        </div>
+        <div className={styles.tab}>
+          해외주식
+        </div>
+      </div>
     </div>
-    );
+  );
 };
 
 export default TopNav;
