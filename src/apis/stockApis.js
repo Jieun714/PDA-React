fbea35ae00a4f57f4e7ed02ea85b8d4b622856ab
Jieun Lee <jieun98.dev@gmail.com>
2023-09-22 13:18:01 +0900

    링크 수정

--- src/apis/stockApis.js
+++ src/apis/stockApis.js
@@ -1,9 +1,16 @@
 import axios from "axios"
 
-const SERVER_URL = "http://localhost:3001"
+// const SERVER_URL = "http://localhost:3001"
+const SERVER_URL =
+        "https://github.com/Jieun714/PDA-React/blob/main/src/data/db.json";
 
 /** 해외주식 이벤트 리스트 GET API 호출 */
 export const getStockGlobalEvents = async () => {
-    const response = await axios.get(`${SERVER_URL}/globalStockEvents`);
-    return response.data;
+    // const response = await axios.get(`${SERVER_URL}/globalStockEvents`);
+    // return response.data;
+    
+    return axios
+        .get(`%{SERVER_URL}`)
+        .then((response) => response.data.globalStockEvents);
 };
+
